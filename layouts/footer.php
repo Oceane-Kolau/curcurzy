9d3cd774c116572c0ffd3b8a28a094eeb42326a5
yann-rzd <yann.rouzaud@gmail.com>
2020-10-08 16:28:24 +0200

    add contact pictures add link on the logo

--- layouts/footer.php
+++ layouts/footer.php
@@ -4,26 +4,25 @@
             <h4>THEY TRUST US</h4>
         </div>
         <div class="michelin">
-            <img class="michelin_logo" src="assetsAli/michelin1.png" alt="michelin">
+            <img class="michelin_logo" src="../assets/icons/footer/michelin.svg" alt="michelin">
         </div>
         <div class="greenweez">
-            <img class="greenweez_logo" src="assetsAli/greenweez.png" alt="greenweez">
+            <img class="greenweez_logo" src="../assets/icons/footer/greenweez.png" alt="greenweez">
         </div>
         <div class="ministre">
-            <img class="ministre_logo" src="assetsAli/ministre.svg.png" alt="badoo">
+            <img class="ministre_logo" src="../assets/icons/footer/agriculture.svg" alt="badoo">
         </div>
         <div class="tinder">
-            <img class="tinder_logo" src="assetsAli/Tinder-logo.png" alt="tinder">
+            <img class="tinder_logo" src="../assets/icons/footer/tinder.png" alt="tinder">
         </div>
     </div>
     <div class="footer">
         <div class="social_network">
-            <a href="#"><img class="insta_logo" src="assetsAli/unnamed.png" alt="insta gram"></a>
-            <a href="#"><img class="fb_logo" src="assetsAli/fb11.png" alt="facebook"></a>
-            <a href="#"><img class="youtube_logo" src="assetsAli/youtube_PNG3.png" alt="you tube"></a>
+            <a href="#"><img class="insta_logo" src="../assets/icons/footer/instagram.png" alt="instagram"></a>
+            <a href="#"><img class="fb_logo" src="../assets/icons/footer/facebook.png" alt="facebook"></a>
         </div>
         <div class="contact_partner">
-            <h4><a href="#">CONTACT</a> | <a href="#">PARTNER</a></h4>
+            <h4><a href="../pages/contact.php">CONTACT | PARTNERS</a></h4>
         </div>
         <div class="copyright">
             <h6>WILD CODE SCHOOL-2020 &copy</h6>
